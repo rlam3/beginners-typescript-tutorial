b63562807ec27b944b073b7419b8292970b2512c
Raymond Lam <raymondlam@peakreservations.com>
2022-11-12 00:08:11 -0500

    Complete exercise 13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -6,7 +6,12 @@ const tryCatchDemo = (state: "fail" | "succeed") => {
       throw new Error("Failure!");
     }
   } catch (e) {
-    return e.message;
+
+    if (e instanceof Error) {
+      return e.message
+    }
+
+    return 'Unknown Error';
   }
 };
 
