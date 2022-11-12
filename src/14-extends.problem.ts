75718c321572bd9547e577dcadda73494d2cdd2e
Raymond Lam <raymondlam@peakreservations.com>
2022-11-12 00:08:11 -0500

    Complete exercise 14

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -6,20 +6,21 @@ import { Equal, Expect } from "./helpers/type-utils";
  * make it more DRY?
  */
 
-interface User {
-  id: string;
+interface IBaseID {
+  id: string
+}
+
+interface User extends IBaseID {
   firstName: string;
   lastName: string;
 }
 
-interface Post {
-  id: string;
+interface Post extends IBaseID{
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends IBaseID{
   comment: string;
 }
 
