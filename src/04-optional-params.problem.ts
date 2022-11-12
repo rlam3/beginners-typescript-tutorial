7a70b732d10d59b722c8620264a304026ed7d282
Raymond Lam <raymondlam@peakreservations.com>
2022-11-12 00:08:11 -0500

    Complete exercise 4

--- src/04-optional-params.problem.ts
+++ src/04-optional-params.problem.ts
@@ -1,6 +1,6 @@
 import { expect, it } from "vitest";
 
-export const getName = (first: string, last: string) => {
+export const getName = (first: string, last?: string): string => {
   if (last) {
     return `${first} ${last}`;
   }
